944c74f1ca95df127f0481c2928c24c59f671d71
GifftyCode <ulokangozi@yahoo.com>
2026-09-16 21:16:58 +0100

    forward ref

--- AdvanceComponents/src/App.tsx
+++ AdvanceComponents/src/App.tsx
@@ -1,10 +1,34 @@
-import Input from "./components/Input";
+// import Input from "./components/Input";
+import { useRef } from "react";
+// import Button from "./components/Button2";
+// import Container from "./components/Container";
+import Input from "./components/InputRef";
 
 function App() {
+  const input = useRef<HTMLInputElement>(null);
   return (
+    // <main>
+    //   <div>
+    //     <Input label="Your Name" id="name" type="text" />
+    //     <Input label="Your Age" id="age" type="number" />
+    //   </div>
+    //   <p>
+    //     <Button el="button">A Button</Button>
+    //   </p>
+    //   <p>
+    //     <Button el="anchor" href="https://google.com">
+    //       A Link
+    //     </Button>
+    //   </p>
+    // </main>
+    // <main>
+    //    <Container as={Button} onClick={() => {}} type="button">
+    //   Click me
+    //  </Container>
+    // </main>
+
     <main>
-      <Input label="Your Name" id="name" type="text" />
-      <Input label="Your Age" id="age" type="number" />
+      <Input label="Test" id="test" ref={input} />
     </main>
   );
 }
